bb6c56b07e759cdb78df73f49947ebaf0559d53b
vnll <91907825+vnll@users.noreply.github.com>
2022-03-13 19:14:32 +0300

    sorting

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react'
+import { useState, useEffect, useMemo } from 'react'
 import './App.css'
 
 const keys = [
@@ -45,26 +45,80 @@ async function getData() {
 
 const App = () => {
   const [data, setData] = useState(null)
+  const [sortingParams, setSortingParams] = useState({
+    key: 'id',
+    order: 'asc',
+  })
+
+  const sortedData = useMemo(() => {
+    if (!data) return data
+
+    const { key, order } = sortingParams
+
+    let result
+
+    if (key === 'id') {
+      result = data
+    } else {
+      result = [...data].sort((item1, item2) =>
+        item1[key].localeCompare(item2[key])
+      )
+    }
+
+    if (order === 'desc') {
+      result = [...result].reverse()
+    }
+
+    return result
+  }, [data, sortingParams])
 
   useEffect(() => {
     getData().then(setData)
   }, [])
 
+  const handleThClick = (key) => {
+    // 1st click
+    if (key !== sortingParams.key) {
+      setSortingParams({
+        key,
+        order: 'asc',
+      })
+    }
+
+    // 2nd
+    else if (sortingParams.order === 'asc') {
+      setSortingParams({
+        key,
+        order: 'desc',
+      })
+    }
+
+    // 3rd
+    else {
+      setSortingParams({
+        key: 'id',
+        order: 'asc',
+      })
+    }
+  }
+
   return (
     <div className="App">
-      {!data ? (
+      {!sortedData ? (
         <div>Loading</div>
       ) : (
         <table>
           <thead>
             <tr>
               {keys.map((key) => (
-                <th key={key}>{key}</th>
+                <th key={key}>
+                  <button onClick={() => handleThClick(key)}>{key}</button>
+                </th>
               ))}
             </tr>
           </thead>
           <tbody>
-            {data.map((item) => (
+            {sortedData.map((item) => (
               <tr key={item.id}>
                 {Object.keys(item).map((key) => (
                   <td key={key}>{item[key]}</td>
