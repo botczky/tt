97b1f895aee2671e93048b43184bf46295082cf8
vnll <91907825+vnll@users.noreply.github.com>
2022-03-13 15:20:11 +0300

    base table

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,79 @@
-import './App.css';
+import { useState, useEffect } from 'react'
+import './App.css'
 
-const App = () => {
-  return (
-    <div className="App">
-      Hello, World!
-    </div>
-  );
+const keys = [
+  'id',
+  'firstName',
+  'lastName',
+  'email',
+  'phone',
+  'street',
+  'city',
+  'state',
+  'zip',
+  'description',
+]
+
+async function getData() {
+  const url =
+    'http://www.filltext.com/?rows=1000&id={number|1000}&firstName={firstName}&delay=3&lastName={lastName}&email={email}&phone={phone|(xxx)xxx-xx-xx}&address={addressObject}&description={lorem|32}'
+
+  const response = await fetch(url)
+  const responseData = await response.json()
+
+  return responseData.map((item) => {
+    const { address, ...rest } = item
+    item = { ...address, ...rest }
+
+    // просто глаза мазолит
+    item.street = item.streetAddress
+    delete item.streetAddress
+
+    const map = new Map()
+
+    for (const key of keys) {
+      map.set(key, item[key])
+    }
+
+    item = Object.fromEntries(map.entries())
+
+    return item
+  })
 }
 
-export default App;
+const App = () => {
+  const [data, setData] = useState(null)
+
+  useEffect(() => {
+    getData().then(setData)
+  }, [])
+
+  return (
+    <div className="App">
+      {!data ? (
+        <div>Loading</div>
+      ) : (
+        <table>
+          <thead>
+            <tr>
+              {keys.map((key) => (
+                <th key={key}>{key}</th>
+              ))}
+            </tr>
+          </thead>
+          <tbody>
+            {data.map((item) => (
+              <tr key={item.id}>
+                {Object.keys(item).map((key) => (
+                  <td key={key}>{item[key]}</td>
+                ))}
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      )}
+    </div>
+  )
+}
+
+export default App
