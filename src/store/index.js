d60ea237159e924ac6e96845116e9616c2ac1bcf
vnll <91907825+vnll@users.noreply.github.com>
2022-03-16 22:50:43 +0300

    intall redux-saga

--- src/store/index.js
+++ src/store/index.js
@@ -1,8 +1,13 @@
 import { createStore, applyMiddleware } from 'redux'
-import thunk from 'redux-thunk'
+import createSagaMiddleware from 'redux-saga'
 import reducer from './reducer'
+import rootSaga from './sagas'
 
-const store = createStore(reducer, applyMiddleware(thunk))
+const sagaMiddleware = createSagaMiddleware()
+
+const store = createStore(reducer, applyMiddleware(sagaMiddleware))
+
+sagaMiddleware.run(rootSaga)
 
 export default store
 export * from './actions'
