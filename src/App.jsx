1a4e1cb557cedcc0f393f0da99dc47ba9b2ff7e6
vnll <91907825+vnll@users.noreply.github.com>
2022-03-21 14:02:05 +0300

    switcher

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,16 @@
+import { useState } from "react";
 import SuperTableContainer from './containers/SuperTableContainer'
+import ColorSchemeSwitcher from './components/ColorSchemeSwitcher'
 import './App.scss'
 
 const App = () => {
+  const [colorScheme, setColorScheme] = useState('dark')
+
   return (
     <div className="App">
+      <div className="App-corner">
+        <ColorSchemeSwitcher value={colorScheme} onChange={setColorScheme} />
+      </div>
       <SuperTableContainer />
     </div>
   )
